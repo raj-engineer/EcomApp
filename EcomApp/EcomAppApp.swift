679e91935fd6f4c61da2b861685ab1009308cb9c
Rajesh <raj.8130052435@gmail.com>
2025-09-18 15:42:00 +0530

    Initial Commit

---
+++ EcomApp/EcomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomAppApp.swift
+//  EcomApp
+//
+//  Created by Rajesh on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EcomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcomAppTests/EcomAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcomAppTests.swift
+//  EcomAppTests
+//
+//  Created by Rajesh on 18/09/25.
+//
+
+import Testing
+@testable import EcomApp
+
+struct EcomAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
